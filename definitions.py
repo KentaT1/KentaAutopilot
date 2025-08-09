1171737259ea670dc79e42ac78458175c93a541d
Kenta Thung <114630801+KentaT1@users.noreply.github.com>
2025-08-08 22:17:59 -0700

    Update definitions.py

--- definitions.py
+++ definitions.py
@@ -1,7 +1,7 @@
 companion_computer = '/dev/ttyACM0' # COM3 (for Windows)
 companion_baud_rate = 115200
 logger_name = 'BEE-EPT-UA913'
-logger_directory = '/home/pi/bee_ept/KentaAutopilot/logs'  # logs (for Windows) 
+logger_directory = '/home/pi/bee-ept/KentaAutopilot/logs'  # logs (for Windows) 
 
 default_roll = 1500
 default_pitch = 1500
@@ -9,3 +9,4 @@ default_yaw = 1500
 default_throttle = 1000
 
 default_servo_aux2 = 1000
+
